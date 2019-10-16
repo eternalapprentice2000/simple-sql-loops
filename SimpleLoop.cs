b69d92943dac6d2eac9082d34dd61b69e22e8fc1
Kenneth Garza <kennethgarza@gmail.com>
2019-10-16 07:33:34 -0400

    Update SimpleLoop.cs

--- SimpleLoop.cs
+++ SimpleLoop.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
@@ -30,6 +29,16 @@ namespace ConsoleApp1
             }
         }
 
+        public static async Task<bool> FakeSqlQuery(Func<bool, bool> CallBack)
+        {
+            var rnd = new Random();
+            var counter = rnd.Next(1, 20);
+
+            await Task.Delay(counter * 1000);
+
+            return CallBack(true);
+        }
+
         public static async Task GetSqlQuery(Func<SqlDataReader, bool> CallBack)
         {
             var sql = "select id, description from Table where something = @value";
@@ -56,6 +65,12 @@ namespace ConsoleApp1
             }
         }
 
+        private static void Log(string msg)
+        {
+            msg = $"[{DateTime.Now.ToString("HH:mm:ss.fff")}] - {msg}";
+            Console.WriteLine(msg);
+        }
+
         static void Main(string[] args)
         {
             var isQueryComplete = true;
@@ -66,52 +81,74 @@ namespace ConsoleApp1
 
             while (loopCounter > 0)
             {
+                Log("Attempting Query Run...");
                 loopCounter--; //decrease by one
 
                 if (isQueryComplete)
                 {
-
+                    Log("Running Query");
                     lock (locker)
                     {
                         isQueryComplete = false;
                     }
                     
-                    Console.WriteLine("Starting Query");
-                    var task = GetSqlQuery((dataReader) =>
-                                {
-                                    // THIS IS THE CALLBACK
-                                    // this is what happens after the query is complete
-                                    // get the result with data reader
+                    Log("Starting Query");
 
-                                    var results = new List<QueryResult>();
+                    // Faked For Testing
+                    Func<bool,bool> fakeCallBack = (b) =>
+                    {
+                        Log("Query Complete");
+                        lock (locker)
+                        {
+                            isQueryComplete = true;
+                        }
+                        
+                        return b;
+                    };
 
-                                    while (dataReader.Read())
-                                    {
-                                        Console.WriteLine("Found Results");
-                                        results.Add(new QueryResult
-                                        {
-                                            Id = dataReader.GetString(0),
-                                            Description = dataReader.GetString(1)
-                                        });
-                                    }
+                    var fakeSqlTask = FakeSqlQuery(fakeCallBack);
 
-                                    // DO SOMETHING WITH THE RESULTS HERE
-                                    lock (locker)
-                                    {
-                                        isQueryComplete = true;
-                                    }
-                                    // do check to see if something went wrong
+                    /*********************
+                     * Real query.. i commented out for testing
 
-                                    return true;
-                                });
+                    Func<SqlDataReader, bool> callback = (dataReader) =>
+                    {
+                        // THIS IS THE CALLBACK
+                        // this is what happens after the query is complete
+                        // get the result with data reader
+                        Log("Found Results");
+                        var results = new List<QueryResult>();
+
+                        while (dataReader.Read())
+                        {
+                            
+                            results.Add(new QueryResult
+                            {
+                                Id = dataReader.GetString(0),
+                                Description = dataReader.GetString(1)
+                            });
+                        }
+
+                        // DO SOMETHING WITH THE RESULTS HERE
+                        lock (locker)
+                        {
+                            isQueryComplete = true;
+                        }
+                        // do check to see if something went wrong
+
+                        return true;
+                    };
+
+                    var task = GetSqlQuery(callback);
+                    ******************/
                 }
                 else
                 {
-                    Console.WriteLine("Skipped Query... the previous query was not complete");
+                    Log("Skipped Query... the previous query was not complete");
 
                 }
 
-                Task.Delay(10 * 1000); // 10 seconds
+                Task.Delay(10 * 1000).GetAwaiter().GetResult(); // 10 seconds
 
             }
         }
